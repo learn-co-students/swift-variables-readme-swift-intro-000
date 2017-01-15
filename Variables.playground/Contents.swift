734fbda0b6ae240a15a58ec5120ee823c0050178
Vitor Ferraz <vitorfvarela@gmail.com>
2017-01-15 18:49:03 -0200

    first class

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,12 +1,5 @@
 var favoriteCharacter = "Jon Snow"
-print(favoriteCharacter)
-
 favoriteCharacter = "Tyrion Lannister"
-print(favoriteCharacter)
-
 favoriteCharacter = 76
-
 let ultimateFavoriteCharacter = "Arya Stark"
-print(ultimateFavoriteCharacter)
-
 ultimateFavoriteCharacter = "Daenerys Targaryen"
