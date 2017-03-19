bb0fa2015f0d8778877dab7441862a82c74966ca
John Doe <dssp@freenet.de>
2017-03-19 17:27:09 +0100

    Added comments

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -4,9 +4,9 @@ print(favoriteCharacter)
 favoriteCharacter = "Tyrion Lannister"
 print(favoriteCharacter)
 
-favoriteCharacter = 76
+favoriteCharacter = 76 // you can not change the type only the value due to type safety
 
 let ultimateFavoriteCharacter = "Arya Stark"
 print(ultimateFavoriteCharacter)
 
-ultimateFavoriteCharacter = "Daenerys Targaryen"
+ultimateFavoriteCharacter = "Daenerys Targaryen" // you can not change the value of the constant. Constants are not mutable. If you want to change value you need to change to a variable.
